29a576e8d6044c03c95d07fe8707230597eb1c06
Mireia <mireia@mireiasangalo.com>
2016-07-24 17:22:44 +0100

    Programme Page added all the functionallity

--- ProgrammePage/programme.js
+++ ProgrammePage/programme.js
@@ -34,17 +34,16 @@ function showCurrentEpisode(jsonData) {
             productionTitle.appendChild(title);
             newDiv.appendChild(productionTitle);
 
-            // Create image + styling
+            // Create image
             var productionImage = document.createElement("img");
-            productionImage.style.width = "60%";
-            productionImage.style.height = "50%";
-            productionImage.style.margin = "auto";
-            productionImage.setAttribute('src', productions[i]._links.image.href);
-            productionImage.setAttribute('alt', "Image of production " + productions[i].channel);
+            productionImage.setAttribute("id", "productionImage");
+            productionImage.setAttribute("src", productions[i]._links.image.href);
+            productionImage.setAttribute("alt", "Image of production " + productions[i].channel);
             newDiv.appendChild(productionImage);
 
             // Create a <p> tag with the corresponding categories
             var categorieName = document.createElement("p");
+            categorieName.setAttribute("id", "categorieName");
             // console.log(productionTitle);
             var caName = document.createTextNode(productions[i].categories);
             categorieName.appendChild(caName);
@@ -52,25 +51,28 @@ function showCurrentEpisode(jsonData) {
 
             // Create a <p> tag with the corresponding duration of the programme
             var programmeDuration = document.createElement("p");
+            programmeDuration.setAttribute("id", "programmeDuration");
             var porDuration = document.createTextNode("Duration: " + productions[i].duration);
             programmeDuration.appendChild(porDuration);
             newDiv.appendChild(programmeDuration);
 
             // Create a <p> tag with the date
             var programmeDate = document.createElement("p");
+            programmeDate.setAttribute("id", "programmeDate");
             var proDate = document.createTextNode(productions[i].broadcastDateTime.displayFormat);
             programmeDate.appendChild(proDate);
             newDiv.appendChild(programmeDate);
 
             // Create a <p> tag with the synopsis
             var programeSynopsis = document.createElement("p");
+            programeSynopsis.setAttribute("id", "programeSynopsis");
             var proSynopsis = document.createTextNode(productions[i].synopsis);
             programeSynopsis.appendChild(proSynopsis);
             newDiv.appendChild(programeSynopsis);
 
             // Create a <p> tag with the availability
             var programmeAvailability = document.createElement("p");
-            programmeAvailability.style.color = "red";
+            programmeAvailability.setAttribute("id", "programmeAvailability");
             var proAvailability = document.createTextNode(productions[i].availabilityEnd.displayFormat);
             programeSynopsis.appendChild(proAvailability);
             newDiv.appendChild(programmeAvailability);
@@ -102,20 +104,50 @@ function showOtherEpisodes(jsonData) {
 
         if (myProgrammeId === programmeID) {
             loopNumberOfEpisodes++;
-            console.log(loopNumberOfEpisodes);
+            // console.log(loopNumberOfEpisodes);
         }
 
         if (myEpisodeId != episodeID && myProgrammeId === programmeID) {
             console.log(productions[i]);
+            var divEpisodeProgramme = document.createElement("div");
+            divEpisodeProgramme.setAttribute("id", "divEpisodeProgramme");
+            fullContent.appendChild(divEpisodeProgramme);
+
+            // Create a href link to this episode
+            var hRef = document.createElement("a");
+            var productionsEpisode = productions[i].episodeId;
+            hRef.setAttribute("border", 0);
+            hRef.setAttribute('href', "?channelId=" + myChannelId + "&episodeId=" +  productionsEpisode + "&programmeId=" + myProgrammeId + "&broadcaster=itv");
+            divEpisodeProgramme.appendChild(hRef);
+
+            // Get Image for episode
+            var episodeImage = document.createElement("img");
+            episodeImage.setAttribute("id", "episodeImage");
+            episodeImage.setAttribute("src", productions[i]._links.image.href);
+            episodeImage.setAttribute("alt", "Image of production " + productions[i].channel);
+            hRef.appendChild(episodeImage);
+
+            // Get title for episode
+            var episodeTitle = document.createElement("h2");
+            episodeTitle.setAttribute("id", "episodeTitle");
+            // console.log(productionTitle);
+            var title = document.createTextNode(productions[i].programmeTitle + " " + productions[i].broadcastDateTime.displayFormat);
+            episodeTitle.appendChild(title);
+            hRef.appendChild(episodeTitle);
         }
     }
 
     // Show this only if loopNumberOfEpisodes is less than 2
     if ( loopNumberOfEpisodes < 2 ) {
+
         // Here we say that there are no more episodes available
-    } else {
-        // Here we inject the content of the other episodes
+        var noEpisodes = document.createElement("p");
+        noEpisodes.setAttribute("id", "noEpisodes");
+        var noEpisodesText = document.createTextNode("No more episondes available.");
+        noEpisodes.appendChild(noEpisodesText);
+        fullContent.appendChild(noEpisodes);
     }
+    document.getElementById("moreEpisodes").appendChild(fullContent);
 }
 
 // global vars that we capture from querystring
